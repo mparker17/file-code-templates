5c29d4bf934130ac6a831e6ebc38d2ef3fbc0acb
M Parker <mparker17@536298.no-reply.drupal.org>
2015-12-31 15:51:03 -0500

    Improve tpl.php template.

--- D7 tpl-php.tpl.php
+++ D7 tpl-php.tpl.php
@@ -1,12 +1,13 @@
 <?php
 /**
  * @file
- * Displays what.
+ * Displays a ${Widget_label}.
  *
  * Available variables:
- * -
+ * - ${DS}variable: An associative array containing:
+ *   - key: Description of value.
  *
- * @see preprocess_function()
+ * @see template_preprocess_${Widget_machine_name}()
  *
  * @ingroup themeable
  */
