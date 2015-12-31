d639ab861e778ba83d3bbb427c323bd9d0a3f39f
M Parker <mparker17@users.noreply.github.com>
2015-12-31 15:16:33 -0500

    Added use strict, attach, detach examples.

--- D7 dot-js file.js
+++ D7 dot-js file.js
@@ -1,6 +1,17 @@
 /**
  * @file
+ * Client-side script for the ${Module_machine_name} module.
  */
 (function ($) {
+
+  "use strict";
+
+  Drupal.behaviors.${Module_machine_name} = {
+    attach: function (context, settings) {
+    },
+
+    detach: function (context, settings, trigger) {
+    }
+  };
 
 })(jQuery);
